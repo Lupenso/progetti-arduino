fb351c940980beafd721b4dc8a188e7efc962968
Lupenso <96111023+Lupenso@users.noreply.github.com>
2021-12-14 11:45:56 +0100

    Update luci_natalizie.ino

--- luci_natalizie/luci_natalizie.ino
+++ luci_natalizie/luci_natalizie.ino
@@ -1,14 +1,16 @@
-int pinVerde1 = 5;
+int pinVerde1 = 3;
 int pinVerde2 = 8;
-int pinVerde3 = 11;
+int pinVerde3 = 1;
 
 int pinRosso1 = 4;
-int pinRosso2 = 7;
-int pinRosso3 = 10;
+int pinRosso2 = 10;
+int pinRosso3 = 11;
 
 int pinBlu1 = 6;
 int pinBlu2 = 9;
-int pinBlu3 = 12;
+int pinBlu3 = 5;
+
+int inputLucine;
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,16 +25,27 @@ void setup() {
   pinMode(pinBlu1, OUTPUT);
   pinMode(pinBlu2, OUTPUT);
   pinMode(pinBlu3, OUTPUT);
+
+  Serial.begin(9600);
+  Serial.println("Scegli il gioco di luci (1-2-3)");
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  lucineOn();
-  delay(500);
-  lucineOff();
+  
+  // lucineOn3();
+  // delay(1000);
+  // lucineOff3();
+
+  // lucineOn2();
+  // delay(200);
+  // lucineOff2();
+  // delay(200);
 }
 
-void lucineOn() {
+
+
+void lucineOn1() {
   // spegne e accende le lucine
   int n = 1;
   if (n < 4) {
@@ -53,7 +66,7 @@ void lucineOn() {
     } 
   }
 }
-void lucineOff() {
+void lucineOff1() {
   int n = 3;
     if (n > 0) {
       if (n == 3) {
@@ -72,4 +85,31 @@ void lucineOff() {
         n--;
       }
     }
+}
+void lucineOn2() {
+  digitalWrite(pinVerde1, HIGH);
+  digitalWrite(pinRosso2, HIGH);
+  digitalWrite(pinBlu3, HIGH);
+}
+void lucineOff2() {
+  digitalWrite(pinVerde1, LOW);
+  digitalWrite(pinRosso2, LOW);
+  digitalWrite(pinBlu3, LOW);
+}
+
+void lucineOn3() {
+  for (int num = 0; num < 255; num++) {
+    analogWrite(pinVerde1, num);
+    analogWrite(pinRosso2, num);
+    analogWrite(pinBlu3, num);
+    delay(10);
+  }
+}
+void lucineOff3() {
+  for (int num = 255; num > 0; num--) {
+    analogWrite(pinVerde1, num);
+    analogWrite(pinRosso2, num);
+    analogWrite(pinBlu3, num);
+    delay(10);
+  }
 }
